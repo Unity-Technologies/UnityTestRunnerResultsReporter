c4635b5b2b9e8299734a078e8cd60057b23a9680
ryanhy <ryanhy@unity3d.com>
2018-12-14 13:56:44 -0800

    write out images and fix test case name for runtime graphics tests

--- TestResultXmlParser.cs
+++ TestResultXmlParser.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
@@ -10,11 +11,14 @@ namespace UnityTestRunnerResultsReporter
     class TestResultXmlParser
     {
         private string testProject = string.Empty;
+        private string resultsPath;
 
         public void ParseTestResults(UnityTestRunnerResultsProcessor processor)
         {
             this.testProject = processor.TestProjectName;
             var testResultXML = TryLoadResultXmlFile(processor.XMLFilePath);
+
+            this.resultsPath = processor.ReportPath;
             
             var testSummary = TryParseTestSummary(testResultXML);
             processor.AddTestSummaryToTestResult(testSummary);
@@ -86,11 +90,38 @@ namespace UnityTestRunnerResultsReporter
         {
             TestCase testCaseResult = new TestCase();
             testCaseResult.name = tc.Attribute("fullname").Value;
+
+            // sometimes test methods can make their own test cases as in the playmode graphics tests
+            // this causes the full names to be the same for all of them, so tack on the case name
+            if (tc.Attribute("name").Value != tc.Attribute("methodname").Value)
+                testCaseResult.name += "." + tc.Attribute("name").Value;
+
             testCaseResult.fixture = this.testProject;
             testCaseResult.state = ConvertToTestState(tc.Attribute("result").Value);
             testCaseResult.time = Convert.ToInt64(Convert.ToDouble(tc.Attribute("duration").Value) * 1000);
             testCaseResult.durationMicroseconds = Convert.ToInt64(Convert.ToDouble(tc.Attribute("duration").Value) * 1000000);
             testCaseResult.className = tc.Attribute("classname").Value;
+
+            var props = tc.Element("properties").Elements();
+            foreach (var prop in props)
+            {
+                if (prop.Attribute("name").Value == "Image" || prop.Attribute("name").Value == "DiffImage")
+                {
+                    var image = Convert.FromBase64String(prop.Attribute("value").Value);
+                    var imageName = prop.Attribute("name").Value == "Image" ? testCaseResult.name + ".png" : testCaseResult.name + "-diff.png";
+                    var imagePath = Path.Combine(resultsPath, "ResultImages");
+
+                    if (!Directory.Exists(imagePath))
+                        Directory.CreateDirectory(imagePath);
+
+                    var fullPath = Path.Combine(imagePath, imageName);
+
+                    File.WriteAllBytes(Path.Combine(fullPath), image);
+
+                    testCaseResult.artifacts.Add(fullPath);
+                }
+            }
+
             if (testCaseResult.state != 4)
             {
                 var messageElement = tc.Descendants("message");
